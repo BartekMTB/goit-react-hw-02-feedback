67d5a61bd004ccc4d1579b90b91b6b7d8ec4b04f
BartekMTB <117111323+BartekMTB@users.noreply.github.com>
2023-06-12 15:02:46 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,26 +7,45 @@ export class App extends Component {
     good: 0,
     neutral: 0,
     bad: 0,
+    total: 0,
   };
 
   static propTypes = {};
+
+  handleOnClickGoodGrade = () => {
+    this.setState({ good: this.state.good + 1, total: this.state.total + 1 });
+  };
+  handleOnClickNeutralGrade = () => {
+    this.setState({
+      neutral: this.state.neutral + 1,
+      total: this.state.total + 1,
+    });
+  };
+  handleOnClickBadGrade = () => {
+    this.setState({ bad: this.state.bad + 1, total: this.state.total + 1 });
+  };
 
   render() {
     return (
       <div className={css.main_div}>
         <h2>Please leave feedback</h2>
         <div>
-          <button>Good</button>
-          <button>Neutral</button>
-          <button>Bad</button>
+          <button onClick={this.handleOnClickGoodGrade}>Good</button>
+          <button onClick={this.handleOnClickNeutralGrade}>Neutral</button>
+          <button onClick={this.handleOnClickBadGrade}>Bad</button>
         </div>
         <h2>Statistics</h2>
         <div>
-          <p>Good: 3</p>
-          <p>Neutral: 2</p>
-          <p>Bad: 2</p>
-          <p>Total: 7</p>
-          <p>Positive feedback: XX%</p>
+          <p>Good: {this.state.good}</p>
+          <p>Neutral: {this.state.neutral}</p>
+          <p>Bad: {this.state.bad}</p>
+          <p>Total: {this.state.total}</p>
+          <p>
+            Positive feedback:
+            {this.state.good > 0 &&
+              ((this.state.good / this.state.total) * 100).toFixed(0)}
+            %
+          </p>
         </div>
       </div>
     );
